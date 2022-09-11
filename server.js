c94b59b7a98d294d02937fe3d0c6ebdedd1eb6ca
ShivaKanchi <shivakanchi111@gmail.com>
2022-09-11 22:21:24 +0530

    Nodemon installed

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const http = require("html");
 const port = 8081;
 
 http.createServer((request, response) => {
-    response.writeHead();
+    response.writeHead(200, { 'Content-type': 'text/html' });
+    response.write('<h1>HelloWorld</h1>');
 });
