f7b2d7f92ccc90f93d0fb1dc3d50122d9c394108
ShivaKanchi <shivakanchi111@gmail.com>
2022-09-11 22:25:01 +0530

    Server script added

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ const port = 8081;
 http.createServer((request, response) => {
     response.writeHead(200, { 'Content-type': 'text/html' });
     response.write('<h1>HelloWorld</h1>');
+    response.end();
+}).listen(port, () => {
+    console.log('Server started at ${port}');
 });
