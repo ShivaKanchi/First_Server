4fa4540556a16270c2df8eb134441490aee050fd
ShivaKanchi <shivakanchi111@gmail.com>
2022-09-17 22:16:34 +0530

    Server script updated

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const http = require('http');
 const port = 8081;
 
+const todolist = ['Music', 'Play'];
 http
-    .createServer((request, response) => {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write('<h1>HelloWorld</h1>');
-        response.end();
+    .createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>HelloWorld</h1>');
+        const { method, url } = req;
+
+        if (url === '/todos') {
+            if (method === 'GET') {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(todolist.toString());
+            } else {
+                res.writeHead(501);
+            }
+        } else {
+            res.writeHead(404);
+        }
+        res.end();
     }).listen(port, () => {
-        console.log('Server started at port', port);
+        console.log(`Server started at ${port}`);
     });
 
     //http://localhost:8081
