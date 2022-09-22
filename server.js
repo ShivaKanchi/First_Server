28072651e6a53aa3ac3dadd9447d5a361b6c0cb8
ShivaKanchi <shivakanchi111@gmail.com>
2022-09-22 22:28:21 +0530

    started express

--- server.js
+++ server.js
@@ -8,13 +8,26 @@ const todolist = ['Music', 'Play'];
 app.get("/todos", (res, req) => {
     res.status(200).send(todolist);
 });
+
 app.post("todos", (res, req) => {
     let newtodolist = req.body.item;
     todolist.push(newtodolist);
     res.status(201).send({
-        message: "Data added"
+        message: `Data ${req.body.item} added`
     });
 });
+
+app.delete("/todos", (re, req) => {
+    const itemtodelete = req.body.item;
+    todolist.find((i, index) => {
+        if (i === itemtodelete) {
+            todolist.splice(index, 1);
+        }
+    });
+    res.status(200).send({
+        message: `Data ${req.body.item} deleted`
+    });
+})
 app.listen(port, () => {
     console.log("Nodejs server started ", port);
 });
