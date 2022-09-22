e75b2288aa931d1a7d1a6997187626380f178894
ShivaKanchi <shivakanchi111@gmail.com>
2022-09-22 22:42:40 +0530

    started express

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ app.use(express.json());
 const port = 8081;
 const todolist = ['Music', 'Play'];
 
-app.get("/todos", (res, req) => {
+app.get("/todos", (req, res) => {
     res.status(200).send(todolist);
 });
 
-app.post("todos", (res, req) => {
+app.post("/todos", (req, res) => {
     let newtodolist = req.body.item;
     todolist.push(newtodolist);
     res.status(201).send({
@@ -17,16 +17,23 @@ app.post("todos", (res, req) => {
     });
 });
 
-app.delete("/todos", (re, req) => {
+app.delete("/todos", (req, res) => {
     const itemtodelete = req.body.item;
     todolist.find((i, index) => {
         if (i === itemtodelete) {
             todolist.splice(index, 1);
         }
     });
-    res.status(200).send({
-        message: `Data ${req.body.item} deleted`
+    res.status(202).send({
+        message: `Data - ${req.body.item} deleted`
     });
+});
+
+app.all("/todos", (req, res) => {
+    res.status(500).send({});
+})
+app.all("*", (req, res) => {
+    res.status(500).send({});
 })
 app.listen(port, () => {
     console.log("Nodejs server started ", port);
